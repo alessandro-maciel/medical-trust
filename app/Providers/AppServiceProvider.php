690bcd402d008828482926fb7cf9286e05c3416e
Alessandro Maciel <alessandromaciel237@gmail.com>
2022-08-20 20:29:34 -0300

    Registrando classe de serviço como uma dependência no contêiner

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Services\MedicalTrust\MedicalTrustService;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->app->singleton(
+            MedicalTrustService::class,
+            fn () => new MedicalTrustService(
+                baseUrl: strval(config('services.medical_trust.url')),
+                apiToken: strval(config('services.medical_trust.token')),
+            ),
+        );
     }
 }
