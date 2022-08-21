ccf1c3df6785c6e81fd5749747e83b61e7615534
Alessandro Maciel <alessandromaciel237@gmail.com>
2022-08-20 21:08:26 -0300

    Adicionando metodos listar todos os registros e adicionar um novo registro

--- app/Services/MedicalTrust/Resources/DentalResource.php
+++ app/Services/MedicalTrust/Resources/DentalResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\MedicalTrust\Resources;
 
+use Illuminate\Http\Client\Response;
 use App\Services\MedicalTrust\MedicalTrustService;
 
 class DentalResource
@@ -9,5 +10,22 @@ class DentalResource
     public function __construct(
         private readonly MedicalTrustService $service,
     ) {
+    }
+
+    public function list(string $identifier): Response
+    {
+        return $this->service->get(
+            request: $this->service->buildRequestWithToken(),
+            url: "/dental/{$identifier}/records",
+        );
+    }
+
+    public function addRecord(string $identifier, array $data = []): Response
+    {
+        return $this->service->post(
+            request: $this->service->buildRequestWithToken(),
+            url: "/dental/{$identifier}/records",
+            payload: $data,
+        );
     }
 }
